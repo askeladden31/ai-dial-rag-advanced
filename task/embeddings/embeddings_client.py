207610a826b46926280665cc44e8484826342460
IH <einherjer@ukr.net>
2026-01-12 16:55:55 +0200

    Implement DialEmbeddingsClient

--- task/embeddings/embeddings_client.py
+++ task/embeddings/embeddings_client.py
@@ -15,7 +15,39 @@ DIAL_EMBEDDINGS = 'https://ai-proxy.lab.epam.com/openai/deployments/{model}/embe
 #   with Embedding model and return back a dict with indexed embeddings (key is index from input list and value vector list)
 
 class DialEmbeddingsClient:
-    ...
+    _endpoint: str
+    _api_key: str
+
+    def __init__(self, deployment_name: str, api_key: str):
+        if not api_key or api_key.strip() == "":
+            raise ValueError("API key cannot be null or empty")
+
+        self._endpoint = DIAL_EMBEDDINGS.format(
+            model=deployment_name
+        )
+        self._api_key = api_key
+
+    def get_embeddings(
+            self, input: list[str],
+            **kwargs
+    ) -> dict:
+
+        headers = {
+            "api-key": self._api_key,
+            "Content-Type": "application/json"
+        }
+        request_data = {
+            "input": input,
+            **kwargs,
+        }
+
+        response = requests.post(url=self._endpoint, headers=headers, json=request_data, timeout=60)
+
+        if response.status_code == 200:
+            data = response.json().get("data", [])
+            return {d["index"]: d["embedding"] for d in data}
+        else:
+            raise Exception(f"HTTP {response.status_code}: {response.text}")
 
 
 # Hint:
